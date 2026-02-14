62987331f3592bc94cddc2e94ad32e377cd0be49
SATHWIK <sathwikssp@gmail.com>
2026-02-14 21:10:36 +0530

    Frontend connected with backend - Full MVP completed

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,49 @@ const express = require("express");
 
 const app = express();
 
+const cors = require("cors");
+
 const PORT = 5000;
 
+app.use(express.json());
+
+app.use(cors());
+
+const messages = [
+  { id: 1, text: "Hello 👋" },
+  { id: 2, text: "How are you?" },
+  { id: 3, text: "This is CHIT-CHAT MVP 🚀" }
+];
+
 app.get("/", (req, res) => {
-    res.send("Hello BOSS 🚀 Server is running");
+  res.json({
+    message: "Backend is working 🚀",
+    status: "success"
+  });
+});
+
+
+app.get("/messages", (req, res) => {
+  res.json(messages);
+});
+
+app.post("/messages", (req, res) => {
+  const { text } = req.body;
+
+  if (!text) {
+    return res.status(400).json({
+      error: "Message text is required"
+    });
+  }
+
+  const newMessage = {
+    id: messages.length + 1,
+    text
+  };
+
+  messages.push(newMessage);
+
+  res.status(201).json(newMessage);
 });
 
 app.listen(PORT, () => {
